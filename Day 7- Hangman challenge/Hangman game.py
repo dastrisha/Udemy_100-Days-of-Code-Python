081217ef16f60345b8b0df97abc6d486732e1448
Trisha Das <das.trisha03@gmail.com>
2024-09-23 22:55:32 -0400

    Checking if the player has won

--- Day 7- Hangman challenge/Hangman game.py
+++ Day 7- Hangman challenge/Hangman game.py
@@ -74,19 +74,35 @@ print(placeholder)
 
 # TODO-1: - Use a while loop to let the user guess again.
 
-guess = input("Guess a letter: ").lower()
 
-display = ""
+#This initializes a boolean variable game_over to False.It's used as a condition for the while loop to keep the game running until the player wins by guessing all letters.
+game_over = False
 
+#An empty list correct_letters is created to store the letters that the player guesses correctly.This will help keep track of the guessed letters and update the displayed word accordingly.
+corrected_letter = []  
 
+#This is a while loop that will keep running as long as game_over is False.The game will continue until the player has guessed all the letters and game_over becomes True.
+while not game_over : 
 
+#The player is prompted to guess a letter using the input() function, which takes input from the user.
+    guess = input("Guess a letter: ").lower()
 
-# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+#The display is an empty string will be updated each time to show the current state of the word with correctly guessed letters.
+    display = ""
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            corrected_letter.append(guess)
+        elif letter in corrected_letter:
+            display += letter
+        else:
+            display+= "_"
 
-print(display)
+    print(display)
+
+    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
